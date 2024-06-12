00280460f0ca6a8b984bca196439f053de2112b5
Davi Vaz <davi.ara.vaz@sempreceub.com>
2024-06-12 20:13:31 -0300

    Alterar senha html feito

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def formulario_login():
 def formulario_cadastrar():
     return render_template("cadastro_usuario.html")
 
+@aplicacao.route("/alterar_senha")
+def formulario_alterar_senha():
+    return render_template("alterar_senha.html")
+
 
 aplicacao.register_blueprint(modulo_usuario)
 aplicacao.register_blueprint(modulo_boletim_ocorrencia)
