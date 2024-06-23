a437c6bb28d95da953d4babbc7c874745cea2000
Davi Vaz <davi.ara.vaz@sempreceub.com>
2024-06-23 18:18:42 -0300

    refactor: Update Flask app configuration and routing
    - Update the SECRET_KEY configuration in the Flask app to use the environment variable.
    - Refactor the routing in the Flask app to use consistent naming conventions and indentation.
    - Add debug mode to the Flask app for development purposes.
    - Remove unused CSS files from the static directory.
    - Add missing CSS files

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ from src.routes.rota_bo import mod_bo
 from src.routes.rota_tst import mod_tst
 
 load_dotenv()
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = SECRET_KEY
+app.config["SECRET_KEY"] = SECRET_KEY
 
-@app.route('/')
+
+@app.route("/")
 def formulario_index():
-  return redirect(url_for('mod_login.login'))
+    return redirect(url_for("mod_login.login"))
+
 
 app.register_blueprint(mod_login)
 app.register_blueprint(mod_bo)
 app.register_blueprint(mod_tst)
 
-if __name__ == '__main__':
-  app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
